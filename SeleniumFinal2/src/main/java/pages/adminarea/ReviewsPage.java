453779705aabd9728b5fb1adb781f889a0d9efd4
nikzhechev <nikolayzhechev87@gmail.com>
2021-12-02 23:12:34 +0200

    The final homework Selenium part 2

--- SeleniumFinal2/src/main/java/pages/adminarea/ReviewsPage.java
+++ SeleniumFinal2/src/main/java/pages/adminarea/ReviewsPage.java
@@ -18,14 +18,12 @@ public class ReviewsPage {
     public static final By EDIT_INPUT_STATUS = By.id("input-status");
     public static final By EDIT_BUTTON_SAVE = By.cssSelector("div.pull-right button.btn-primary");
     public static final By ASSERT_FOR_SUCCESS_ENABLE_REVIEWS = By.cssSelector("div.alert-success");
-    public static final By ELEMENT_FOR_DELETE = By.xpath("//*[@id=\"form-review\"]/div/table/tbody/tr/td[1]/input");
-    public static final By BUTTON_DELETE = By.cssSelector("div.pull-right button.btn-danger");
-    public static final By ASSERT_FOR_SUCCESS_DEL_REVIEWS = By.cssSelector("div.alert-success");
+
 
     /**
      * This method search reviews in filter for search
      *
-     * @param authorName accept param of String for product Name
+     * @param authorName        accept param of String for product Name
      * @param messagesOnFailure message that will appear in your reports in case of failure
      */
     public static void searchReviewsImac(String authorName, String messagesOnFailure) {
@@ -53,7 +51,7 @@ public class ReviewsPage {
     /**
      * This method Asserts if the validation message is contained when reviews is approved
      *
-     * @param expMessages the expected validation message
+     * @param expMessages       the expected validation message
      * @param messagesOnFailure message that will appear in your reports in case of failure
      */
     public static void verifyReviewsImacItem(String expMessages, String messagesOnFailure) {
@@ -74,24 +72,5 @@ public class ReviewsPage {
         Assert.assertTrue(Browser.driver.findElement(ASSERT_FOR_AUTHOR).getText().contains(authorName), messagesOnFailure);
     }
 
-    /**
-     * This method delete reviews from admin page
-     */
-    public static void deleteReviewsImac() {
-        Browser.driver.findElement(ELEMENT_FOR_DELETE).click();
-        Browser.driver.findElement(BUTTON_DELETE).click();
-        Browser.driver.switchTo().alert().accept();
 
-
-    }
-
-    /**
-     * This method Asserts if the validation message is contained when delete reviews
-     * @param expMessages the expected validation message
-     * @param messagesOnFailure message that will appear in your reports in case of failure
-     */
-    public static void verifyDeleteReviews(String expMessages, String messagesOnFailure) {
-        Assert.assertTrue(Browser.driver.findElement(ASSERT_FOR_SUCCESS_DEL_REVIEWS).getText().contains(expMessages), messagesOnFailure);
-
-    }
 }
